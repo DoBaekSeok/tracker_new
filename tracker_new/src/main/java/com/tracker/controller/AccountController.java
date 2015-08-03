6e80bbdafa01db20c2e21bdd978700572597a5d7
soy <soy@3c22>
2015-08-03 15:49:04 +0900

    contoller/주석

--- tracker_new/src/main/java/com/tracker/controller/AccountController.java
+++ tracker_new/src/main/java/com/tracker/controller/AccountController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tracker.model.dto.Member;
 import com.tracker.service.MemberService;
@@ -28,32 +27,6 @@ public class AccountController {
 	public void setMemberService(MemberService memberSerivce){
 		this.memberService = memberSerivce;
 	}
-/*	@RequestMapping(value = "login.action", method=RequestMethod.POST)
-	@ResponseBody
-	public Member login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		Member member = null;
-		String id = req.getParameter("id");
-		String password = req.getParameter("password");
-		
-		if(id == null || id.length() == 0 || password == null || password.length() == 0){
-			return member;
-		}
-		
-		member = memberService.getMemberByIdAndPasswd(id, password);
-		
-		resp.setContentType("text/plain;charset=utf-8");
-		PrintWriter writer = resp.getWriter();
-		
-		//로그인 session 처리 
-		if(member != null){
-			req.getSession().setAttribute("loginuser", member);
-			writer.print("success");
-		}else{
-			writer.print("fail");
-		}
-		
-		return member;
-	}*/
 	
 	@RequestMapping(value = "login.action", method=RequestMethod.POST)
 	public void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
