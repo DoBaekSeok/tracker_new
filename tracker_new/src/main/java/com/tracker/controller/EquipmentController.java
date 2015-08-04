a9e70783b2f192ba2b6e50fd8397a98b91176c47
geonshik <geonshik@3c14>
2015-08-04 14:22:32 +0900

    mapper.xml

--- tracker_new/src/main/java/com/tracker/controller/EquipmentController.java
+++ tracker_new/src/main/java/com/tracker/controller/EquipmentController.java
@@ -48,7 +48,7 @@ public class EquipmentController{
 		String oper = req.getParameter("oper");
 		
 		if(oper.equals("add")){			
-			System.out.println("111111111111111111");
+
 			
 			Equipment equipment = new Equipment();
 			String equipName = req.getParameter("equipName");
@@ -68,7 +68,7 @@ public class EquipmentController{
 
 		}else if(oper.equals("edit")){
 			
-			System.out.println("222222222222222222222");
+
 			Equipment equipment = new Equipment();
 			int equipNo = Integer.parseInt(req.getParameter("equipNo"));
 			String equipName = req.getParameter("equipName");
