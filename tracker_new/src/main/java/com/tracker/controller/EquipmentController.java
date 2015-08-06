ad5177c452b593c4bf2aac839d6d8033b21fa26f
geonshik <geonshik@3c14>
2015-08-06 11:11:23 +0900

    controller

--- tracker_new/src/main/java/com/tracker/controller/EquipmentController.java
+++ tracker_new/src/main/java/com/tracker/controller/EquipmentController.java
@@ -44,15 +44,9 @@ public class EquipmentController{
 	
 	@RequestMapping("equipmenteditajax.action")
 	public void getEquipmentedit(HttpServletRequest req, HttpServletResponse resp) {
-		
-		//가상경로를 물리경로로 변환하는 기능을 가진 객체 반환
-		//ServletContext application = req.getSession().getServletContext();
-	    
-		//가상경로 -> 물리경로
-		//String path = application.getRealPath("/resources/img/product");
+
 		
 		Equipment equipment = new Equipment();
-		//EquipPhoto equipphoto = new EquipPhoto();
 		String oper = req.getParameter("oper");
 		
 		if(oper.equals("add")){			
@@ -61,18 +55,15 @@ public class EquipmentController{
 	        String modelName = req.getParameter("modelName");
 	        int equipPrice = Integer.parseInt(req.getParameter("equipPrice"));
 	        String equipContent = req.getParameter("equipContent");  
-	       // String savedFileName = req.getParameter("equipPhoto");
-	       //System.out.println(savedFileName);
+
 
 			equipment.setEquipName(equipName);
 			equipment.setModelName(modelName);
 			equipment.setEquipPrice(equipPrice);
 			equipment.setEquipContent(equipContent);
-			//equipphoto.setSavedFileName(savedFileName);
 
 			equipmentService.AddEquipment(equipment);
 
-			///////////////////////////////////////////////////////////////// add insertEquipment
 
 		}else if(oper.equals("edit")){
 			
@@ -94,57 +85,6 @@ public class EquipmentController{
 			
 		}
 		
-		
-//		@RequestMapping(value = "photoregister.action", method = RequestMethod.POST)
-//		public String photoregisterEdit(MultipartHttpServletRequest req, String id, String title, String content) {
-//			//가상경로를 물리경로로 변환하는 기능을 가진 객체 반환
-//			ServletContext application = req.getSession().getServletContext();
-//					
-//			//가상경로 -> 물리경로
-//			String path = application.getRealPath("/resources/imagefile/");
-//			
-//			Board board = new Board();
-//			board.setBdWriter(id);
-//			board.setBdTitle(title);
-//			board.setBdContent(content);
-//			System.out.println(id+"/"+title+"/"+content);
-//			boardService.insertPhoto(board);
-//			MultipartFile file = req.getFile("img");//요청 데이터에서 파일 정보 추출
-//			if (file != null && file.getSize() > 0) {
-//				
-//				String fileName = file.getOriginalFilename();//파일이름 읽어서 변수에 저장
-//				if (fileName.contains("\\")) {//IE일 경우 전체 경로에서 파일이름만 추출
-//					//C:\ABC\DEF\xyz.txt -> xyz.txt
-//					fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
-//				}
-//				String savedName = Util.getUniqueFileName(path, fileName);
-//				BoardImage boardImage = new BoardImage();
-//				
-//				boardImage.setBdImgName(savedName);
-//				boardImage.setBdNo(board.getBdNo());
-//				
-//				boardService.insertPhotoImage(boardImage);
-//				
-//				//파일을 디스크에 저장
-//				try {
-//					FileOutputStream ostream = 
-//						new FileOutputStream(new File(path, boardImage.getBdImgName()));
-//					InputStream istream = file.getInputStream();
-//					while (true) {
-//						int data = istream.read();
-//						if (data == -1) break;
-//						ostream.write(data);
-//					}
-//					istream.close();
-//					ostream.close();
-//				} catch (Exception ex) {
-//					ex.printStackTrace();
-//				}
-//			}
-//
-//			return "redirect:/board/photolist.action";
-//		}
-//		
 
 	}
 }
